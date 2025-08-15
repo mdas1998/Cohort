15c57167c8d1f8ada82f90e90b4799040cf71bda
Micheal Das <gokumicheal81@gmail.com>
2025-08-15 14:19:46 +0530

    feat: Landing Page created

--- Assignments/Week 1/7 - Landing Pages/Shopping/main.js
+++ Assignments/Week 1/7 - Landing Pages/Shopping/main.js
@@ -12,3 +12,40 @@ navLinks.addEventListener('click', (e) => {
   navLinks.classList.remove('open');
   menuBtnIcon.setAttribute('class', 'ri-menu-line');
 });
+
+const scrollRevealOption = {
+  distance: '50px',
+  duration: 1000,
+  origin: 'bottom'
+}
+
+ScrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
+  origin: 'right',
+});
+
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header__content p", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".header__content form", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".header__content .bar", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".header__image__card", {
+  duration: 1000,
+  interval: 500,
+  delay: 2200,
+});
